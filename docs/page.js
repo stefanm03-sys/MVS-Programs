d520b47d17e6c2184c4388d00477eeea5158da98
stefanm03-sys <stefanmmolina03@gmail.com>
2026-03-21 21:26:12 -0400

    Button

--- docs/page.js
+++ docs/page.js
@@ -1,4 +1,6 @@
 // This is a JS comment
 addEventListener(MouseEvent.CLICK, function() {
-    alert("Cicked!");
-}); // This code adds an event listener for a click event. When an element is clicked, it will display an alert with the message "Cicked!"
+    if (btn1.clicked) {
+        alert("Cicked!");
+    }
+}); // This code adds an event listener for a click event. When the button is clicked, it will display an alert with the message "Cicked!"
