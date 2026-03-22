0fc76669f4f73ad9491d6840cab85a9799ca27f1
stefanm03-sys <stefanmmolina03@gmail.com>
2026-03-22 14:35:49 -0400

    More Button

--- docs/page.js
+++ docs/page.js
@@ -1,6 +1,10 @@
 // This is a JS comment
-addEventListener(MouseEvent.CLICK, function() {
-    if (btn1.clicked) {
-        alert("Cicked!");
-    }
-}); // This code adds an event listener for a click event. When the button is clicked, it will display an alert with the message "Cicked!"
+const btn1 = document.getElementById("btn1"); // This selects the button element with the id "btn1" and assigns it to the variable btn1
+btn1.addEventListener("click", () => { // This adds an event listener to the button that listens for a "click" event from the mouse, and executes the function when the button is clicked
+    btn1.textContent = "Clicked!"; // This changes the text content of the button to "Clicked!" when it is clicked using bt1. for the variable and textContent to change the text
+    alert("You clicked the button!"); // A defaut page alert that can pop up
+});
+
+
+
+
