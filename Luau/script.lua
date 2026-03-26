33fc8828f5337ba683e2eff11e6cf72109b6b17b
Stefan Molina <stefanmmolina03@gmail.com>
2026-03-26 19:58:28 +0000

    Small Edit

--- Luau/script.lua
+++ Luau/script.lua
@@ -1,1 +1,7 @@
 -- Will be empty for the moment
+print("Luau") -- Luau uses the primary Lua functions. Regular Lua can be used inside of a Luau script together
+
+for i = 0, 3 do -- Iterates through 0 and 3
+    print(i) -- Prints the numeric value
+end -- Closee the block
+
