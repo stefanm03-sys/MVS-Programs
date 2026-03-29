8666d269211d695815e4865ce46a23d0e1c301f5
Stefan Molina <stefanmmolina03@gmail.com>
2026-03-29 04:58:30 +0000

    Examples

--- Luau/script.lua
+++ Luau/script.lua
@@ -1,7 +1,29 @@
--- Will be empty for the moment
-print("Luau") -- Luau uses the primary Lua functions. Regular Lua can be used inside of a Luau script together
+-- This is a comment in Luau
+--[[
+    This is a multi line comment
+    Read import.lua for the base lua code!
+]]
 
-for i = 0, 3 do -- Iterates through 0 and 3
-    print(i) -- Prints the numeric value
-end -- Closee the block
+local Players = game:GetService("Players") -- This is how you fetch a Roblox Service in Luau. "Players" is the service to manage player instances
 
+local function onHumanoidDied(player)
+    print(player.Name .. "'s humanoid died!")
+end
+
+local function onCharacterAdded(player, character)
+    local humanoid = character:WaitForChild("Humanoid")
+    humanoid.Died:Connect(function()
+        onHumanoidDied(player)
+    end)
+end
+
+local function onPlayerAdded(player)
+    print("Player Joined: " .. player.Name)
+    player.CharacterAdded:Connect(function(char)
+        onCharacterAdded(player, char)
+    end)
+end
+
+Players.PlayerAdded:Connect(onPlayerAdded)
+
+print("Luau demo script loaded (server)!")
